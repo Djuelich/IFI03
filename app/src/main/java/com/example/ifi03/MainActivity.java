5262d6374dac5f0b9b322b2579eccf1dd3ca1c92
djuelich <djuelich@uni-osnabrueck.de>
2021-05-15 16:55:22 +0200

    created splashscreen with Logo and switchingfunction to MainMenu created MainMenu activity

--- app/src/main/java/com/example/ifi03/MainActivity.java
+++ app/src/main/java/com/example/ifi03/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.ifi03;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                setContentView(R.layout.activity_main_menu);
+            }
+        }, 3000); //the time you want to delay in milliseconds
     }
 }
