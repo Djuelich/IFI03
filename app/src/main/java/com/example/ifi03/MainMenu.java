366abcd064f4262e2b591d5ebfb51dfeafea318b
djuelich <djuelich@uni-osnabrueck.de>
2021-05-18 21:41:24 +0200

    changed views

--- app/src/main/java/com/example/ifi03/MainMenu.java
+++ app/src/main/java/com/example/ifi03/MainMenu.java
@@ -1,42 +1,29 @@
 package com.example.ifi03;
 
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toolbar;
 
-public class MainMenu extends AppCompatActivity implements View.OnClickListener{
+public class MainMenu extends AppCompatActivity{
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu_1);
 
-        Button button1 = findViewById(R.id.button_2lj);
-        Button button2 = findViewById(R.id.button_3lj);
-        Button button3 = findViewById(R.id.button3);
-        Button button4 = findViewById(R.id.button4);
-        Button button5 = findViewById(R.id.button5);
-
-        button1.setOnClickListener(this);
-        button2.setOnClickListener(this);
-        button3.setOnClickListener(this);
-        button4.setOnClickListener(this);
-        button5.setOnClickListener(this);
-    }
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
 
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.button_2lj:
-                setContentView(R.layout.activity_main_menu2);
-                break;
-            case R.id.button1:
-                setContentView(R.layout.activity_main_menu3);
-                break;
-
-        }
     }
 }
